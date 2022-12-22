b4708a9fb1a72c7c855dcb6ca124ffa9200b15e4
RodrigoYujiKogaKuroda <yujikuroda.rykk@gmail.com>
2022-12-21 21:02:38 -0300

    refactor: Changed 'Modelagem de Banco (Agência de Viagens)' query

--- Prática - Modelagem de Banco (Agência de Viagens)/1.sql
+++ Prática - Modelagem de Banco (Agência de Viagens)/1.sql
@@ -1,46 +1,21 @@
-CREATE TABLE "public.companies" (
+CREATE TABLE "companies" (
 	"id" serial NOT NULL,
 	"name" TEXT NOT NULL UNIQUE,
-	"acronym" TEXT NOT NULL UNIQUE,
-	CONSTRAINT "companies_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+	"acronym" TEXT NOT NULL UNIQUE
 );
 
-
-
-CREATE TABLE "public.flights" (
+CREATE TABLE "flights" (
 	"id" serial NOT NULL,
 	"acronym" TEXT NOT NULL UNIQUE,
-	"company_id" integer NOT NULL,
+	"company_id" integer NOT NULL REFERENCES "companies"("id"),
 	"departure" TIMESTAMP NOT NULL,
 	"arrival" TIMESTAMP NOT NULL,
-	"origin_id" integer NOT NULL UNIQUE,
-	"destination_id" integer NOT NULL UNIQUE,
-	CONSTRAINT "flights_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+	"origin_id" integer NOT NULL REFERENCES "airports"("id"),
+	"destination_id" integer NOT NULL REFERENCES "airports"("id"),
 );
 
-
-
-CREATE TABLE "public.airports" (
+CREATE TABLE "airports" (
 	"id" serial NOT NULL,
 	"name" TEXT NOT NULL UNIQUE,
-	"acronym" TEXT NOT NULL UNIQUE,
-	CONSTRAINT "airports_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+	"acronym" TEXT NOT NULL UNIQUE
 );
-
-
-
-
-ALTER TABLE "flights" ADD CONSTRAINT "flights_fk0" FOREIGN KEY ("company_id") REFERENCES "companies"("id");
-ALTER TABLE "flights" ADD CONSTRAINT "flights_fk1" FOREIGN KEY ("origin_id") REFERENCES "airports"("id");
-ALTER TABLE "flights" ADD CONSTRAINT "flights_fk2" FOREIGN KEY ("destination_id") REFERENCES "airports"("id");
-
-
-
-
-
